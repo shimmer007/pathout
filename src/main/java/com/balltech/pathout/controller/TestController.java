164875004650990588ee047cb9942db0aacfc6b8
BoscoLeet <787534184@qq.com>
2023-04-30 15:15:30 +0800

    Description:添加data的业务代码供帅和光学习使用 Author:Liu

--- src/main/java/com/balltech/pathout/controller/TestController.java
+++ src/main/java/com/balltech/pathout/controller/TestController.java
@@ -1,14 +1,12 @@
 package com.balltech.pathout.controller;
 
 import com.balltech.pathout.User.User;
-import com.balltech.pathout.database.model.ContentValues;
 import com.balltech.pathout.database.utils.DbUtil;
 import com.balltech.pathout.highlight.HighLight;
-import com.google.protobuf.StringValue;
-import com.mysql.cj.log.Log;
-import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.log4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
@@ -17,8 +15,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
-
-import org.apache.log4j.Logger;
 
 @RestController
 public class TestController {
@@ -48,5 +44,24 @@ public class TestController {
     @GetMapping("/highList")
     public List<HighLight> getHighLight() {
         return new ArrayList<>();
+    }
+
+    @GetMapping("/data")
+    public String getUserDataSheet(@RequestParam("name") String user_id) {
+        HikariDataSource dataSource = new HikariDataSource(DbUtil.getConfig());
+        try (Connection connection = dataSource.getConnection()) {
+            String dataSheetResult = "";
+            PreparedStatement statement = connection.prepareStatement("SELECT * FROM playerdatasheet WHERE user_id ='" + user_id + "'");
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                dataSheetResult = dataSheetResult + resultSet.getString(1) +" "+ resultSet.getString(2)
+                    +" "+ resultSet.getString(3) + " "+resultSet.getString(4) +" "+ resultSet.getString(5);
+                dataSheetResult = dataSheetResult + ";   ";
+            }
+            return dataSheetResult;
+        } catch (SQLException e) {
+            System.out.println("xsaxsa");
+        }
+        return "";
     }
 }
